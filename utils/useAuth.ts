0596b2bcca59eb03363dc7c1574f54be9044b0dd
Budi <budimanfajarf@gmail.com>
2023-09-11 12:31:48 +0700

    Remove buggy codes

--- utils/useAuth.ts
+++ utils/useAuth.ts
@@ -14,9 +14,6 @@ export function useAuth() {
 
   socket?.on('disconnect', () => {
     console.info(`Socket disconnected: ${socket?.id}`);
-
-    // Logout user when the socket disconnect
-    logout();
   });
 
   useEffect(() => {
