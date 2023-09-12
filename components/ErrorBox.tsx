5c972a2da7f0d7338101eddb6c2781a8fb0b07ed
Budi <budimanfajarf@gmail.com>
2023-09-13 01:01:21 +0700

    Update error box

--- components/ErrorBox.tsx
+++ components/ErrorBox.tsx
@@ -3,7 +3,7 @@ export const ErrorBox = ({ context = 'Error', error }: { error: string; context?
     error && (
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
         <div
-          className="bg-red-50 border border-red-300 text-red-500 px-4 py-3 rounded relative"
+          className="bg-red-50 border border-red-300 text-red-500 px-6 py-4 rounded-xl relative"
           role="alert"
         >
           <strong className="font-medium">{context}: </strong>
