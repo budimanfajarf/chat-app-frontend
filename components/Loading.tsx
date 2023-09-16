e84494fa06ccae9ffee058e60bba6872c90e48f1
Budi <budimanfajarf@gmail.com>
2023-09-17 03:13:09 +0700

    Update LoadingSpinner

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 export const LoadingSpinner = ({ isLoading }: { isLoading: boolean }) => {
+  const [showWaitMessage, setShowWaitMessage] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout | null = null;
+
+    if (isLoading) {
+      timeout = setTimeout(() => {
+        setShowWaitMessage(true);
+      }, 5000); // 5 seconds
+    }
+
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [isLoading]);
+
+  if (!isLoading) return null;
+
   return (
-    <div
-      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${
-        isLoading ? 'block' : 'hidden'
-      }`}
-    >
-      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-slate-500"></div>
+    <div className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center z-50">
+      <div className="mt-8 animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-slate-500"></div>
+      <div
+        className={`text-sm mt-4 px-4 text-center text-slate-500 ${
+          showWaitMessage ? 'opacity-100 transition-opacity duration-300 ease-in-out' : 'opacity-0'
+        }`}
+      >
+        Please wait for a few minutes. <br />
+        This backend service using free cloud, it may take some time to re-start the service.
+      </div>
     </div>
   );
 };
