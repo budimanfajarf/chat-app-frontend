ad4880642cf2db95f05d2a8558f6a601c89953f6
Budi☕ <budimanfajarf@gmail.com>
2025-09-17 17:37:44 +0700

    feat: update by link

--- components/RootLayout.tsx
+++ components/RootLayout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://budi.day/"
+            href="https://card.budi.day/"
             target="_blank"
             rel="noopener noreferrer"
           >
